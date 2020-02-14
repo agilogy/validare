8206c67b51352c59272fa3f96828344da8e3296c
Jordi Pradel <jordi.pradel@agilogy.com>
2020-02-14 15:28:32 +0100

    Add support for cats

--- core/src/main/scala/com/agilogy/validare/utils/Indexable.scala
+++ core/src/main/scala/com/agilogy/validare/utils/Indexable.scala
@@ -2,13 +2,34 @@ package com.agilogy.validare.utils
 
 import scala.language.higherKinds
 
-trait Indexable[-S[_]] extends HasLength[S[_]]{
+import cats.Foldable
+import cats.implicits._
+
+trait Indexable[-S[_]] extends HasLength[S[_]] {
   def zipWithIndex[A](values: S[A]): Traversable[(A, Int)]
-  def at[A](s:S[A], index:Int):Option[A]
+  def at[A](s: S[A], index: Int): Option[A]
 }
 
-object Indexable {
-  def apply[S[_] : Indexable]: Indexable[S] = implicitly[Indexable[S]]
+trait IndexableLowLevel {
+
+  implicit def foldableIndexable[F[_]: Foldable] = new Indexable[F] {
+    override def zipWithIndex[A](values: F[A]): List[(A, Int)] =
+      values
+        .foldLeft((0, List.empty[(A, Int)])) {
+          case ((idx, result), a) => (idx + 1, (a, idx) :: result)
+        }
+        ._2
+
+    override def at[A](s: F[A], index: Int): Option[A] = Foldable[F].get(s)(index.toLong)
+
+    override def length(value: F[_]): Int = Foldable[F].size(value).toInt
+  }
+
+}
+
+object Indexable extends IndexableLowLevel {
+
+  def apply[S[_]: Indexable]: Indexable[S] = implicitly[Indexable[S]]
 
   implicit val seqIndexable: Indexable[Seq] =
     new Indexable[Seq] {
@@ -19,4 +40,5 @@ object Indexable {
 
       override def length(value: Seq[_]): Int = value.size
     }
+
 }
