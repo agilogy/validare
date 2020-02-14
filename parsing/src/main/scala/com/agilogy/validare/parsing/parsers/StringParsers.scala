3ed452c0b1583f376029a3955f937c7ecf10f082
Jordi Pradel <jordi.pradel@agilogy.com>
2020-02-14 15:28:22 +0100

    Remove unused modules

--- parsing/src/main/scala/com/agilogy/validare/parsing/parsers/StringParsers.scala
+++
@@ -1,7 +0,0 @@
-package com.agilogy.validare.parsing.parsers
-
-import com.agilogy.validare.validation.predicates.Predicates
-
-trait StringParsers extends SimpleStringParsers with StringTransformations with Predicates
-
-object StringParsers extends StringParsers
