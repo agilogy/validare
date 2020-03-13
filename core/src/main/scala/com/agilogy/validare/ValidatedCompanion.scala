204616a601bab253bf23765c20c7454f21f28634
Jordi Pradel <jordi.pradel@agilogy.com>
2020-03-13 10:36:07 +0100

    Make ValidatedCompanionLike a trait for its user, not a base trait for implementations

--- core/src/main/scala/com/agilogy/validare/ValidatedCompanion.scala
+++ core/src/main/scala/com/agilogy/validare/ValidatedCompanion.scala
@@ -4,28 +4,25 @@ import scala.reflect.ClassTag
 
 import cats.implicits._
 
+import com.agilogy.validare.ValidatedCompanion.ValidatedCompanionLike
 import com.agilogy.validare.validation.{ Parser, Predicate, Property }
 
 final case class ValidationError[A, B](typeName: String, failsPredicate: Predicate[B]) extends Exception {
   override def getMessage: String = s"Error validating $typeName. The value fails to satisfy $failsPredicate."
 }
 
-trait ValidatedCompanionLike[A, B] {
-
-  def predicate: Predicate[A]
-  def unsafe(value: A): B
-  def typeName: String
+abstract class ValidatedCompanion[A, B: ClassTag](validation: Predicate[A])(build: A => B)
+    extends ValidatedCompanionLike[A, B] {
+  def predicate: Predicate[A] = validation
+  def unsafe(value: A): B     = build(value)
+  def typeName: String        = implicitly[ClassTag[B]].toString()
 
   final def parser: Parser[A, B] = Parser.of(predicate, Property(typeName, unsafe))
 
-  def apply(value: A): Either[ValidationError[B, A], B] =
+  override def apply(value: A): Either[ValidationError[B, A], B] =
     parser.parse(value).leftMap(i => ValidationError(typeName, i.failing))
-
 }
 
-abstract class ValidatedCompanion[A, B: ClassTag](validation: Predicate[A])(build: A => B)
-    extends ValidatedCompanionLike[A, B] {
-  override def predicate: Predicate[A] = validation
-  override def unsafe(value: A): B     = build(value)
-  override def typeName: String        = implicitly[ClassTag[B]].toString()
+object ValidatedCompanion {
+  type ValidatedCompanionLike[A, B] = A => Either[ValidationError[B, A], B]
 }
